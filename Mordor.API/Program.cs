7da25d9f4431f07e5a048e67cb1b7310c11d4a72
Anatoly Sergov <Anatolysergov@gmail.com>
2024-05-28 10:05:09 -0400

    updated cors

--- Mordor.API/Program.cs
+++ Mordor.API/Program.cs
@@ -10,8 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddSingleton<DapperDbContext>(provider =>
 {
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    return new DapperDbContext(configuration);
+	var configuration = provider.GetRequiredService<IConfiguration>();
+	return new DapperDbContext(configuration);
 });
 
 // Register UserRepository as a scoped service
@@ -23,7 +23,7 @@ builder.Services.AddControllers();
 // Configure Entity Framework DbContext
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddEndpointsApiExplorer();
@@ -34,11 +34,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCors();
 app.MapControllers();
 app.Run();
