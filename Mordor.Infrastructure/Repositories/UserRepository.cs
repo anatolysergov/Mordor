b51fbc984b95556f51f068141c348d9af253e849
Anatoly Sergov <Anatolysergov@gmail.com>
2024-05-28 10:15:08 -0400

    updated userRepo to use connection manager

--- Mordor.Infrastructure/Repositories/UserRepository.cs
+++ Mordor.Infrastructure/Repositories/UserRepository.cs
@@ -5,58 +5,49 @@ using Mordor.Infrastructure.Data;
 
 namespace Mordor.Infrastructure.Repositories
 {
-    public class UserRepository : IUserRepository
-    {
-        private readonly DapperDbContext _context;
+	public class UserRepository : IUserRepository
+	{
+		private readonly DapperDbContext _context;
+		private readonly ConnectionManager _connectionManager;
 
-        public UserRepository(DapperDbContext context)
-        {
-            _context = context;
-        }
+		public UserRepository(DapperDbContext context, ConnectionManager connectionManager)
+		{
+			_context = context;
+			_connectionManager = connectionManager;
+		}
 
-        public async Task<AppUser> GetUserById(Guid userId)
-        {
-            using (var connection = _context.CreateConnection())
-            {
-                var sql = "SELECT * FROM Users WHERE UserId = @UserId";
-                return await connection.QueryFirstOrDefaultAsync<AppUser>(sql, new { UserId = userId });
-            }
-        }
+		public async Task<AppUser> GetUserById(Guid userId)
+		{
+			var sql = "SELECT * FROM Users WHERE UserId = @UserId";
+			return await _connectionManager.Connection.QueryFirstOrDefaultAsync<AppUser>(sql, new { UserId = userId });
+		}
 
-        public async Task<AppUser> GetUserByUsername(string username)
-        {
-            using (var connection = _context.CreateConnection())
-            {
-                var sql = "SELECT * FROM Users WHERE Username = @Username";
-                return await connection.QueryFirstOrDefaultAsync<AppUser>(sql, new { Username = username });
-            }
-        }
+		public async Task<AppUser> GetUserByUsername(string username)
+		{
 
-        public async Task CreateUser(AppUser user)
-        {
-            using (var connection = _context.CreateConnection())
-            {
-                var sql = "INSERT INTO Users (UserId, UserName, Password) VALUES (@UserId, @UserName, @Password)";
-                await connection.ExecuteAsync(sql, user);
-            }
-        }
+			var sql = "SELECT * FROM Users WHERE Username = @Username";
+			return await _connectionManager.Connection.QueryFirstOrDefaultAsync<AppUser>(sql, new { Username = username });
+		}
 
-        public async Task UpdateUser(AppUser user)
-        {
-            using (var connection = _context.CreateConnection())
-            {
-                var sql = "UPDATE Users SET UserName = @UserName, Password = @Password WHERE UserId = @UserId";
-                await connection.ExecuteAsync(sql, user);
-            }
-        }
+		public async Task CreateUser(AppUser user)
+		{
 
-        public async Task DeleteUser(Guid id)
-        {
-            using (var connection = _context.CreateConnection())
-            {
-                var sql = "DELETE FROM Users WHERE UserId = @UserId";
-                await connection.ExecuteAsync(sql, new { Id = id });
-            }
-        }
-    }
+			var sql = "INSERT INTO Users (UserId, UserName, Password) VALUES (@UserId, @UserName, @Password)";
+			await _connectionManager.Connection.ExecuteAsync(sql, user);
+		}
+
+		public async Task UpdateUser(AppUser user)
+		{
+
+			var sql = "UPDATE Users SET UserName = @UserName, Password = @Password WHERE UserId = @UserId";
+			await _connectionManager.Connection.ExecuteAsync(sql, user);
+		}
+
+		public async Task DeleteUser(Guid id)
+		{
+
+			var sql = "DELETE FROM Users WHERE UserId = @UserId";
+			await _connectionManager.Connection.ExecuteAsync(sql, new { Id = id });
+		}
+	}
 }
